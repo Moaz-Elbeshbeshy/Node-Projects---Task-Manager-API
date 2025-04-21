d4d53366846a1f177dedb158793994d745c2043a
Moaz Elbeshbeshy <awladnasem800@gmail.com>
2025-04-21 13:03:30 +0000

    added dot env file

--- starter/app.js
+++ starter/app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
 
-const port = 3000
 
 // middleware
 app.use(express.json())
@@ -16,9 +17,15 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasks)
 
+const port = 3000
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        console.log('Connected to database ....')
+        app.listen(port, console.log(`port is listening on port ${port}....`))
+    } catch (err) {
+        console.log(err)
+    }
+}
 
-
-
-
-
-app.listen(port, console.log(`port is listening on port ${port}....`))
+start()
