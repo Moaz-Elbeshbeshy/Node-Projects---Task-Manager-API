f0299ca424cf6023e02972a4bba0c4792dfe1dd9
Moaz Elbeshbeshy <awladnasem800@gmail.com>
2025-04-21 06:28:13 +0000

    created two tasks.js files in the controller and the routes to keep app.js clean

---
+++ starter/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+
+const port = 3000
+
+// middleware
+app.use(express.json())
+
+
+// routes
+app.get('/hello', (req, res) => {
+    console.log('Hello')
+    res.send('Hello')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+
+
+
+
+app.listen(port, console.log(`port is listening on port ${port}....`))
